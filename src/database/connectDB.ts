b2fc506895ffaca40c293391f6a25175c991d724
YoursBuddy <syedhamza8972@gmail.com>
2025-05-08 00:54:37 +0530

    Change the Structure

--- src/database/connectDB.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import { mongoURL } from "../config.js";
-import { string } from "zod";
-
-export async function connectToDatabase(){
-    const connection = await mongoose.connect(String(mongoURL));
-}
--- src/routes/link.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { auth } from "../middlewares/authorized.js";
-import { newContent } from "../controller/link.controller.js";
-import { contentValidator } from "../validator/content.js";
-
-export const linkRouter = Router();
-
-
-linkRouter.post("/create",auth,contentValidator,newContent)
